0f4e50b22390b264f9b768ba94503c49c8f9ae75
d4nQw3rty <danlinmoy@gmail.com>
2023-01-18 13:41:27 -0400

    Add associations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  has_many :likes, foreign_key: :post_id
+  has_many :comments, foreign_key: :post_id
+  belongs_to :author, class_name: 'User', foreign_key: :author_id  
 end
