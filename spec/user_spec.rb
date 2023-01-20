534d101e9cd89000683ddd967861d42bc8c6f61a
d4nQw3rty <danlinmoy@gmail.com>
2023-01-20 08:33:24 -0400

    Update user_spec.rb
    -Check name not to be nill
    -Check return the correct quantity of post
    -Check the post counter is greater or equal to zero to be valid

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   subject do
-    User.create(name: 'Name', photo: 'Photo', bio: 'Bio')
+    User.create(id:1, name: 'Name', photo: 'Photo', bio: 'Bio')
   end
 
   before { subject.save }
 
-  describe 'Validations' do
+  descrwithout a name' do
+      subject.name = nil
+      expect(subject).ibe 'Validations' do
     it 'is valid with valid attributes' do
       expect(subject).to be_valid
     end
 
-    it 'is not valid without a name' do
-      subject.name = nil
-      expect(subject).to_not be_valid
+    it 'is not valid to_not be_valid
     end
 
     it 'post_counter have to be bigger than 0' do
@@ -26,6 +26,11 @@ RSpec.describe User, type: :model do
     it 'post_counter have to be greater or iqual to zero' do
       subject.posts_counter = 0
       expect(subject).to be_valid
+    end
+
+    it 'latest_posts method should return the last 3 posts' do
+      4.times { Post.create(title: 'Title', text: 'Text', author_id: subject.id) }
+      expect(subject.latest_posts.count).to eq(3)
     end
   end    
 end  
