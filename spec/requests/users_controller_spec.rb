bb334e7c7d6610304ff62df61a39f3af42317348
d4nQw3rty <danlinmoy@gmail.com>
2023-01-25 10:37:55 -0400

    Fix test issues

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,33 +1,44 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :request do
-  describe 'GET /index' do
-    it 'returns http index success' do
-      get('/users')
-      expect(response).to have_http_status(:success)
+RSpec.describe "Users", type: :request do
+  before(:each) do
+    @user = User.create(name: 'Dan Linares', photo: 'https://unsplash.com/photos/mateopic',
+                        bio: 'Developer, Venezuelan and Musician', posts_counter: 5)
+  end
+
+  describe "request list of all users" do
+    before(:each) { get users_path(@user) }
+
+    it "Gives the correct response status" do
+      expect(response).to have_http_status(:ok)
     end
-    it 'returns http index success' do
-      get('/users')
-      expect(response).to render_template(:index)
+
+    it 'Does not render show template' do
+      expect(response).to_not render_template(:show)
     end
-    it 'ensures the body to include the correct placeholder text' do
-      get('/users')
-      expect(response.body).to include('Welcome to user index page')
+
+    it "Renders the correct template" do
+      expect(response).to render_template('index')
+    end
+
+    it "Checks for user list info into the body" do
+      expect(response.body).to include("Dan Linares")
     end
   end
 
-  describe 'GET /show' do
-    it 'returns http show success' do
-      get '/users/:id', params: { id: 1 }
-      expect(response).to have_http_status(:success)
+  describe "request to show a specific user info" do
+    before(:each) { get user_path(@user) }
+
+    it 'Gives the exact response' do
+      expect(response).to have_http_status(:ok)
     end
-    it 'returns render template Show' do
-      get '/users/:id', params: { id: 1 }
-      expect(response).to render_template(:show)
+
+    it 'Renders the correct template' do
+      expect(response).to render_template('show')
     end
-    it 'ensures the body to include the correct placeholder text' do
-      get '/users/:id', params: { id: 1 }
-      expect(response.body).to include('These are a user for a Id')
+
+    it "Checks for single user info into the body" do
+      expect(response.body).to include("Dan Linares")
     end
   end
 end
