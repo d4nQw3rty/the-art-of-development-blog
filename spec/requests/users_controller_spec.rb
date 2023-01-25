50ed219d9e01df0bbcadbecb18e591cdee2f73c6
d4nQw3rty <danlinmoy@gmail.com>
2023-01-25 10:42:48 -0400

    Fix Linters

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe 'Users', type: :request do
   before(:each) do
     @user = User.create(name: 'Dan Linares', photo: 'https://unsplash.com/photos/mateopic',
                         bio: 'Developer, Venezuelan and Musician', posts_counter: 5)
   end
 
-  describe "request list of all users" do
+  describe 'request list of all users' do
     before(:each) { get users_path(@user) }
 
-    it "Gives the correct response status" do
+    it 'Gives the correct response status' do
       expect(response).to have_http_status(:ok)
     end
 
@@ -17,16 +17,16 @@ RSpec.describe "Users", type: :request do
       expect(response).to_not render_template(:show)
     end
 
-    it "Renders the correct template" do
+    it 'Renders the correct template' do
       expect(response).to render_template('index')
     end
 
-    it "Checks for user list info into the body" do
-      expect(response.body).to include("Dan Linares")
+    it 'Checks for user list info into the body' do
+      expect(response.body).to include('Dan Linares')
     end
   end
 
-  describe "request to show a specific user info" do
+  describe 'request to show a specific user info' do
     before(:each) { get user_path(@user) }
 
     it 'Gives the exact response' do
@@ -37,8 +37,8 @@ RSpec.describe "Users", type: :request do
       expect(response).to render_template('show')
     end
 
-    it "Checks for single user info into the body" do
-      expect(response.body).to include("Dan Linares")
+    it 'Checks for single user info into the body' do
+      expect(response.body).to include('Dan Linares')
     end
   end
 end
