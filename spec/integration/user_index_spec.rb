8845adfebf4d6d4a313c390f061108fa52219adf
d4nQw3rty <danlinmoy@gmail.com>
2023-01-30 18:01:26 -0400

    Add test to check user index
    * I can see the username of all other users.
    * I can see the profile picture for each user.
    * I can see the number of posts each user has written.
    * When I click on a user, I am redirected to that user's show page.

--- spec/integration/user_index_spec.rb
+++ spec/integration/user_index_spec.rb
@@ -1,11 +1,52 @@
 require 'rails_helper'
 
-RSpec.describe 'Hello world', type: :system do
+RSpec.describe 'User Index', type: :system do
   describe 'index page' do
-    it 'shows the right content' do
-      visit root_path
-
-      expect(page).to have_content('Welcome to user index page')
+    before(:each) do
+      @user = User.create(
+        name: 'Kanza Tehreen',
+        bio: 'Full Stack Web Developer',
+        photo: 'https://avatars.kanzatehreen.com',
+        posts_counter: 1
+      )
+  
+      @user2 = User.create(
+        name: 'Camilo Vela',
+        bio: 'Web Developer',
+        photo: 'https://avatars.camilovela.com',
+        posts_counter: 2
+      )
     end
+
+    it 'should display all users' do
+      visit users_path
+      expect(page).to have_content('Kanza Tehreen')
+      expect(page).to have_content('Camilo Vela')
+    end
+    
+    it 'shows the right photo' do
+      visit users_path
+      expect(page).to have_css("img[src*='https://avatars.kanzatehreen.com']")
+      expect(page).to have_css("img[src*='https://avatars.camilovela.com']")
+    end
+
+    it 'displays the right Number of posts' do
+      visit users_path
+      expect(page).to have_content('Number of Posts: 1')
+      expect(page).to have_content('Number of Posts: 2')
+    end
+
+    it 'redirected to show path' do
+      visit users_path
+      click_link 'Kanza Tehreen'
+      expect(page).to have_content('Full Stack Web Developer')      
+    end
+    
+    it 'redirected to show path' do
+      visit users_path
+      click_link 'Camilo Vela'
+      expect(page).to have_content('Web Developer')      
+    end
+
   end
 end
