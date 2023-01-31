fb1d6b9a57fe2416296372d457ab61de5b888e1b
d4nQw3rty <danlinmoy@gmail.com>
2023-01-31 09:56:09 -0400

    Update post_show_spec.rb

--- spec/integration/post_show_spec.rb
+++ spec/integration/post_show_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-RSpec.describe 'Posts Show', type: :feature do
+RSpec.describe 'Posts Show', type: :system do
   describe 'post show page' do
     before(:each) do
       @user = User.create(
@@ -31,38 +31,43 @@ RSpec.describe 'Posts Show', type: :feature do
     end
 
     describe 'Displays the entire body comments counters, and title' do
-      it 'displays the counters' do
-        visit user_post_path(@user, @post)
-        expect(page).to have_content('Comments: 3')
-        expect(page).to have_content('Likes: 1')
-      end
-
-      it 'displays post title' do
+      
+      it 'displays post title' do #1
         visit user_post_path(@user, @post)
         expect(page).to have_content('First Step')
       end
 
-      it 'displays post by author as the title' do
+      it 'displays post author' do #2
         visit user_post_path(@user, @post)
-        expect(page).to have_content('First Step by Kanza Tehreen')
+        expect(page).to have_content('Kanza Tehreen')
       end
 
-      it 'displays the posts content' do
+      it 'displays the  comments counters' do #3
+        visit user_post_path(@user, @post)
+        expect(page).to have_content('Comments: 3')       
+      end
+
+      it 'displays the likes counter' do #4
+        visit user_post_path(@user, @post)
+        expect(page).to have_content('Likes: 1')
+      end
+
+      it 'displays the posts body' do #5
         visit user_post_path(@user, @post)
         expect(page).to have_content('This is my first post')
       end
 
-      it 'checks there is a comment section' do
+      it 'displays the the username of each commentor' do #6
         visit user_post_path(@user, @post)
-        expect(page).to have_css("ul[class*='post_list comments_box_border']")
-        expect(page).to have_content('Comments')
+        expect(page).to have_content('Kanza Tehreen')
+        expect(page).to have_content('Camilo Vela')
       end
-
-      it 'displays all comments with usernames' do
+    
+      it 'displays all comments each commentor left' do #7
         visit user_post_path(@user, @post)
-        expect(page).to have_content('Kanza Tehreen: A lot of people are hungry')
-        expect(page).to have_content('Kanza Tehreen: Nothing is impossible')
-        expect(page).to have_content('Camilo Vela: Can you help me?')
+        expect(page).to have_content('A lot of people are hungry')
+        expect(page).to have_content('Nothing is impossible')
+        expect(page).to have_content('Can you help me?')
       end
     end
   end
