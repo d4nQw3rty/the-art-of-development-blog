3d88133dead60ffab5e2e48e6e93a5164dac3bb7
d4nQw3rty <danlinmoy@gmail.com>
2023-02-01 15:45:07 -0400

    Fix log in Issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,10 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base 
+ 
   before_action :authenticate_user!
-  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
-
-  def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :bio, :photo, :email, :password_confirmation, :password)
-    end
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :email, :bio, :photo, :password, :current_password)
-    end
-  end
-
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-      user_params.permit(:username, :email)
-    end
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name password password_confirmation])
   end
 end
